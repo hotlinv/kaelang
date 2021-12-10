2e7bc9732997ea285236996b9685f64546004b6b
lilin <lilin@ascs.tech>
2021-12-10 22:09:23 +0800

    feat:添加列表拼接

--- kalib/列表.kp.py
+++ kalib/列表.kp.py
@@ -2,7 +2,7 @@
 ka_pmap=lambda:{
 	u"列表：(\w+)到(\w+)":"ka_range({0}, {1})",
     u"在《(.+)》中插入：(.+)":"ka_append('{0}', '{1}')",
-    u"将列表《(.+)》拼接在一起":"ka_join({0})"
+    u"将列表《(.+)》(?:进行)?拼接":"ka_join('{0}')"
 }
 
 # 【实现】
@@ -10,10 +10,13 @@ def ka_range(b, e):
     return range(b, e+1)
 
 def ka_join(lst):
-    return "".join(lst)
+    ft = f"ka_vals['{lst}拼接后']=''.join([str(i) for i in ka_vals['{lst}']])"
+    exec(compile(ft, "list_join", "exec"))
+    
 
 def ka_append(name, lst):
     ls = lst.split("、")
     lst2 = [eval(eval(f"run('{co}')")) for co in ls]
     ft = f"ka_vals['{name}'].extend({lst2})"
     exec(compile(ft, "list_append", "exec"))
+    
