bf6e87a6108d3979b079ed5138f45495f7f3e6df
lilin <lilin@ascs.tech>
2021-12-16 15:11:56 +0800

    feat:添加图像上绘制多边形的功能

--- kalib/图像.kp.py
+++ kalib/图像.kp.py
@@ -7,22 +7,48 @@ ka_pmap=lambda:{
     u"(?:将|改变)图(?:像|片)《(.+)》(?:改|转)?为“(\w+)”模式":"ka_pil_convert('{0}', '{1}')",
     u"(?:显示|展示)图(?:像|片)《(.+)》":"ka_pil_show('{0}')",
     u"将图(?:像|片)《(.+)》另存为：(.+)":"ka_pil_save('{0}', {1})",
+    u"在图(?:像|片)《(.+)》上用色刷《(.+)》绘制多边形图案《(.+)》":"ka_pil_draw_polygon('{0}', '{1}', '{2}')",
 }
 
 # 【实现】
 from PIL import Image
 from PIL import ImageDraw
-import os
+import os, math
 
 _ka_imgmode=re.compile(u"模式为：“(\w+)”")
 _ka_imgsize=re.compile(u"(?:大小|尺寸)?为：(?:横|长)?：(\d+)(?:像素)*?(?:，)?\s*竖：(\d+)(?:像素)*")
+_modes = {"二值图":"1", "灰度图":"L", "索引表图":"P", "真色彩图":"RGB", "带透明真色彩图":"RGBA"}
+_colors = {"白色":(255,255,255,255), "黑色":(0,0,0,255), "红色":(255, 0, 0, 255), "绿色":(0, 255, 0, 255), "蓝色":(0, 0, 255, 255)}
 
+
+@catch2cn
+def getmode(name):
+    if name in _modes:
+        return _modes[name]
+    else:
+        return name
+
+@catch2cn
+def getcolor(name):
+    if name in _colors:
+        return _colors[name]
+    else:
+        return name
+
+@catch2cn
 def ka_new_empty_image(name, value):
+    """新建空图像"""
     mm = _ka_imgmode.findall(value)
     sm = _ka_imgsize.findall(value)
-    ka_vals[name]=Image.new(mm[0], [int(s) for s in sm[0]])
+    ka_vals[name]=Image.new(getmode(mm[0]), [int(s) for s in sm[0]])
+
+@catch2cn
+def ka_new_fillcolor(name, value):
+    """新建画刷"""
+    ka_vals[name]=getcolor(value)
 
 registType("空图像", ka_new_empty_image)
+registType("色刷", ka_new_fillcolor)
 
 @catch2cn
 def ka_pil_open(path, name):
@@ -47,4 +73,22 @@ def ka_pil_save(name, path):
 @catch2cn
 def ka_pil_convert(name, mode):
     """图像模式转换"""
-    ka_vals[name] = ka_vals[name].convert(mode)
+    ka_vals[name] = ka_vals[name].convert(getmode(mode))
+
+@catch2cn
+def ka_pil_draw_polygon(imgname, colorname, polyname):
+    """图像上绘制多边形"""
+    img = ka_vals[imgname]
+    color = ka_vals[colorname]
+    poly = ka_vals[polyname]
+    if img.mode=="1":
+        color = tuple([math.ceil(color[0]/255)])
+    elif img.mode=="L":
+        color = tuple(color[:1])
+    elif img.mode=="RGB":
+        color = tuple(color[:3])
+    else:
+        color = tuple(color)
+    #print(img.mode, color)
+    pdraw = ImageDraw.Draw(img)
+    pdraw.polygon([tuple(p) for p in poly] ,fill=color)
--- kalib/文件.kp.py
+++ kalib/文件.kp.py
@@ -1,14 +1,35 @@
 # 【映射】
 ka_pmap=lambda:{
 	u"^当前目录$":"ka_workspace()",
+    u"从“(.+)”(?:读取|加载)(?:本地)?(json|yaml)文件(?:，)?(?:并)?(?:将其命名为)?“(.+)”":"ka_loadfile('{1}', '{0}', '{2}')"
 }
 
 # 【实现】
-import os
+import os, json, yaml
 
 @catch2cn
 def ka_workspace():
     """获取当前目录"""
     return os.path.abspath(os.path.curdir)
 
+@catch2cn
+def ka_loadjson(path, name):
+    """加载json文件"""
+    with open(path, "r") as read_file:
+        j = json.load(read_file)
+        ka_vals[name] = j
 
+@catch2cn
+def ka_loadyaml(path, name):
+    """加载文件（yaml或json）"""
+    f = open(path)
+    y = yaml.load(f, Loader=yaml.FullLoader)
+    ka_vals[name] = y
+
+@catch2cn
+def ka_loadfile(type, path, name):
+    """加载文件（yaml或json）"""
+    if type=="json":
+        ka_loadjson(path, name)
+    elif type=="yaml":
+        ka_loadyaml(path, name)
