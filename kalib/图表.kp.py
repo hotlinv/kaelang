20f608064546db3e404051f5a09b6c9f57b63059
lilin <lilin@ascs.tech>
2021-12-22 18:21:11 +0800

    feat:添加matplotlib的支持

---
+++ kalib/图表.kp.py
@@ -0,0 +1,20 @@
+# 【引用】matplotlib
+
+# 【映射】
+ka_pmap=lambda:{
+	u"在图板上(?:显示|展示|绘制)图片《(.+)》":"ka_plot_show_img('{0}')",
+}
+
+# 【实现】
+import matplotlib.pyplot as plt
+matplotlib.rc("font",family='FangSong')
+
+@catch2cn
+def ka_plot_show_img(name):
+    """图板上显示图像"""
+    plt.figure(name) # 图像窗口名称
+    plt.imshow(ka_vals[name])
+    plt.axis('on') # 关掉坐标轴为 off
+    plt.title(name) # 图像题目
+    plt.show()
+
