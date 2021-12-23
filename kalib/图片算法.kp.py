b50c6fe8dae0b6589a51da2c201ba0bf94eec138
lilin <lilin@ascs.tech>
2021-12-23 18:35:57 +0800

    feat:添加双图对比和最小值滤波

--- kalib/图片算法.kp.py
+++ kalib/图片算法.kp.py
@@ -1,15 +1,28 @@
-# 【引用】PIL、numpy、ima
+# 【引用】PIL、numpy、scipy
 
 # 【映射】
 ka_pmap=lambda:{
-	u"将图(?:像|片)《(.+)》切割成多个矩形“(.+)”":"ka_img_splitrect('{0}', '{1}')"
+	u"将图(?:像|片)《(.+)》切割成多个矩形“(.+)”":"ka_imga_splitrect('{0}', '{1}')",
+    u"对图像《(.+)》运行最小值滤波操作，(.+)":"ka_imga_minimum_filter('{0}', '{1}')"
 }
 
 # 【实现】
 from PIL import Image
 from PIL import ImageDraw
-import ima
+from scipy import ndimage, misc
+# import ima
 
-def ka_img_splitrect(imgname, rectname):
-    img = ka_vals[imgname]
-    print(ima.splitRects(img))
+def ka_imga_splitrect(imgname, rectname):
+    # img = ka_vals[imgname]
+    # print(ima.splitRects(img))
+    pass
+
+_ka_imga_window_size=re.compile(u"窗口大小为(\d+)")
+
+def ka_imga_minimum_filter(imgname, param):
+    """图像最小滤波操作"""
+    ws = _ka_imga_window_size.findall(param)
+    #print(ws)
+    # ascent = misc.ascent()
+    result = ndimage.minimum_filter(ka_vals[imgname], size=int(ws[0]))
+    ka_vals[imgname+"最小滤波后"]=result
--- kalib/图表.kp.py
+++ kalib/图表.kp.py
@@ -2,7 +2,8 @@
 
 # 【映射】
 ka_pmap=lambda:{
-	u"在图板上(?:展示|绘制)图像《(.+)》":"ka_plot_show_img('{0}')",
+    u"在图板上(?:展示|绘制)图像《(.[^《》、]+)》、《(.[^《》、]+)》":"ka_plot_show_2img('{0}','{1}')",
+    u"在图板上(?:展示|绘制)图像《(.+)》":"ka_plot_show_img('{0}')",
 }
 
 # 【实现】
@@ -18,3 +19,16 @@ def ka_plot_show_img(name):
     plt.title(name) # 图像题目
     plt.show()
 
+@catch2cn
+def ka_plot_show_2img(name1, name2):
+    """图板上显示2张图像"""
+    fig = plt.figure("双图对比") # 图像窗口名称
+    if ka_vals[name1].mode=="L":
+        plt.gray()
+    ax1 = fig.add_subplot(121)  # left side
+    ax2 = fig.add_subplot(122)  # right side
+    ax1.imshow(ka_vals[name1])
+    #ax1.title(name1) # 图像题目
+    ax2.imshow(ka_vals[name2])
+    #ax2.title(name2) # 图像题目
+    plt.show()
