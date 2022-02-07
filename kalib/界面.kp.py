4afb78c13134111bbffbb69901065551512a2416
lilin <lilin@ascs.tech>
2022-02-07 17:54:29 +0800

    feat:试试往树控件加结点

--- kalib/界面.kp.py
+++ kalib/界面.kp.py
@@ -6,6 +6,17 @@ ka_pmap=KaeLevMap(lev0={
 })
 
 # 【实现】
+from kivy.uix.treeview import TreeView, TreeViewLabel
+
+class FileTree(TreeView):
+    def __init__(self, **kwargs):
+        self.register_event_type('on_create')
+        super(FileTree, self).__init__(**kwargs)
+        self.dispatch('on_create')
+    def on_create(self):
+        n1 = self.add_node(TreeViewLabel(text='项 1', font_name='fzh'))
+        self.add_node(TreeViewLabel(text='SubItem 1', font_name='fzh'), n1)
+        self.add_node(TreeViewLabel(text='SubItem 2', font_name='fzh'), n1)
 
 @catch2cn
 def ka_gui_open(uiconfig):
@@ -17,13 +28,14 @@ def ka_gui_open(uiconfig):
     
     kvLabelBase.register(name='fzh',fn_regular='.asserts/sarasa.ttf')
 
-    with open(uiconfig, encoding="utf-8") as f:
-        uiconf = f.read()
-
-    root = kvBuilder.load_string(uiconf)
-
     class TestApp(kvApp):
         def build(self):
+            with open(uiconfig, encoding="utf-8") as f:
+                uiconf = f.read()
+
+            root = kvBuilder.load_string(uiconf)
+            self.icon = root.app_icon
+            self.title = root.app_title
             return root
     
     app = TestApp()
