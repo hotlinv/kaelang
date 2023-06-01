d95ed597aca40a60c8e378f7b61d3f502641a27f
lilin <linux_23@163.com>
2023-06-01 14:38:41 +0800

    feat:为readme添加双击运行说明

--- regwin.bat
+++ regwin.bat
@@ -12,7 +12,7 @@ echo %py%
 reg add "HKEY_CLASSES_ROOT\.ae" /t REG_SZ /d kaelang
 reg add "HKEY_CLASSES_ROOT\kaelang" /t REG_SZ /d Kae脚本文件
 reg add "HKEY_CLASSES_ROOT\kaelang\DefaultIcon" /t REG_SZ /d %~dp0.asserts\favicon64.ico
-reg add "HKEY_CLASSES_ROOT\kaelang\shell\open\command" /t REG_SZ /d "%py%/Scripts/kae.exe \"%%1\""
+reg add "HKEY_CLASSES_ROOT\kaelang\shell\open\command" /t REG_SZ /d "%py%Scripts/kae.exe \"%%1\""
 echo 注册表修改完成.
 
 echo.
